32dd5fe30e09b47d51e1832637f5db683b563623
lowagner <lowagner@gmail.com>
2014-12-31 09:51:42 +0100

    rearranged config to show relevant display stuff for low-res screens

--- defaultconfig.py
+++ defaultconfig.py
@@ -10,37 +10,25 @@ FLUIDSYNTHdriver = "alsa"
 # display resolution
 DEFAULTresolution = (960,540)
 
+# The following affect the appearance of things during play/edit:
 # size of the white / black keys
 WHITEKEYfraction = 0.13 # fraction of the size of the vertical screen
 BLACKKEYwhitefraction = 0.7 # fraction of the size of a white key
-
-FONT = "monospace"
-FONTSIZEmultiplier = 1
-DIVIDERcolor = (100,120,80)
-MEASUREcolor = (10,0,10)
-
-# SETTINGS FOR PLAY AND EDIT
-
-# click track or metronome settings
-METRONOMEdefault = False    # whether to default to a metronome/clicktrack or not
-CLICKTRACKvolume = 0.05     # number from 0 to 1
-
-# PLAY SETTINGS
-SANDBOXplay = 1 # default whether to play sandbox mode or not.  can change in game.
-DEFAULTdifficulty = 0
-
-# PIANO KEYBOARD SETTINGS - how big, what colors, etc.
-# rainbow colors for the keyboard.  starts on C, then chromatic up to B
-# in the (R,G,B) format, values from 0 to 255 inclusive.
-#               C           C#        D         Eb              E           F
-rainbow = [ (255,0,0), (245, 40, 0), (210,80,0), (190,130,0), (220,220,0), (140,228,0), 
-            (0,240,0), (0,159,199), (0,55,248), (33,0,252), (114,0,228), (180,0,180) ]
-#               F#          G           Ab          A           Bb          B
-# technically it can be whatever colors you want, without it being a rainbow.
-# The following affect the appearance of the keys and notes during regular play/edit:
 NOTEwidth = 20
 KEYwidth = 50
-PIXELSperbeat = 100
+# how big (vertically) is a beat:
+PIXELSperbeat = 100     # lower this for a low-res screen
+FONT = "monospace" # what font to use
+FONTSIZEmultiplier = 1  # change this to 1.5 or 2 for low-res screens
+# how do measures look:
+DIVIDERcolor = (100,120,80)
+MEASUREcolor = (10,0,10)
+# number of lines to show in the helper box on edit mode:
+HELPERLINEmax = 5
+# what color is the cursor in edit mode:
+CURSORcolor = (100,250,250)
+
+# NON-DISPLAY SETTINGS
 
 # where to find the songs:
 PIECEdirectory = "songs"
@@ -48,7 +36,11 @@ PIECEdirectory = "songs"
 RESOURCEdirectory = "resources"
 
 # MORE SOUND SETTINGS
-# instrument bank.  originally copied from /usr/share/sounds/sf2/FluidR3_GM.sf2,
+
+# click track or metronome settings:
+METRONOMEdefault = False    # whether to default to a metronome/clicktrack or not
+CLICKTRACKvolume = 0.05     # number from 0 to 1
+# instrument bank:  originally copied from /usr/share/sounds/sf2/FluidR3_GM.sf2,
 # but then I found an augmented version online.  Search the internet for it:
 SOUNDfont = "FluidR3_GM-2.sf2"  # put it in your RESOURCEdirectory.
 #SOUNDfont = "The_Nes_Soundfont.sf2"  # you can also make chiptunes if you like.
@@ -88,21 +80,35 @@ PIANOchannel = 0
 # Default instrument
 DEFAULTinstrument = 0
 
+
+# PLAY SETTINGS
+SANDBOXplay = 1 # default whether to play sandbox mode or not.  can change in game.
+DEFAULTdifficulty = 0
+
+# PIANO KEYBOARD SETTINGS - how big, what colors, etc.
+# rainbow colors for the keyboard.  starts on C, then chromatic up to B
+# in the (R,G,B) format, values from 0 to 255 inclusive.
+#               C           C#        D         Eb              E           F
+rainbow = [ (255,0,0), (245, 40, 0), (210,80,0), (190,130,0), (220,220,0), (140,228,0), 
+            (0,240,0), (0,159,199), (0,55,248), (33,0,252), (114,0,228), (180,0,180) ]
+#               F#          G           Ab          A           Bb          B
+# technically it can be whatever colors you want, without it being a rainbow.
+
+
 # EDIT SETTINGS
-COMMANDhistory = 100 # number of lines to keep
-HELPERLINEmax = 5
-CURSORcolor = (100,250,250)
+COMMANDhistory = 100 # number of lines to keep in the history
 # defaults for new compositions
 # get the tempo and stuff from the midi file.
 PLAYERstarts = True # true for solo piano pieces, false when there is background music
 ALLOWEDplayertracks = [0] # which track does the player have to play?  
 ALLOWEDdifficulties = [0] # list of allowed difficulties
-TEMPO = 120
+TEMPO = 120 # default tempo
 DEFAULTplayertrack = 0
 
+# it is not recommended to mess with the following, but you can if you know what you're doing:
 # resolution = # of ticks per beat.  
 EDITresolution = 5040   # (1 * 2 * 3 * 2 * 5 * 7 * 2 * 3 * 2 * 2 * 2)/4 
-                        # if EDITresolution = 5040, and there are four beats per measure,
+                        # if EDITresolution = 5040, and if there are four beats per measure,
                         # this means we can represent these various notes by these numbers of ticks:
                         # NOTE            # TICKS
                         # whole            20,160
@@ -120,7 +126,8 @@ EDITresolution = 5040   # (1 * 2 * 3 * 2 * 5 * 7 * 2 * 3 * 2 * 2 * 2)/4
 EDITshortestnote = 315  # shortest note you want to allow, in ticks.
 EDITnotespace = 20 # # of ticks to give white-space letting a note go and hitting the next
 
-# GAME relevant variables
+
+# GAME relevant variables:  don't mess with these.
 # GAMESTATEexit = 0 # exit the game, MUST BE ZERO, so no choice :)
 GAMESTATEplay = 1 # playing a piece
 GAMESTATEpiecesettings = 2  # setting various settings for the piece -- tempo, etc.
